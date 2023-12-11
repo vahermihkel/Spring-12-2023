57a3ac74dfc6b6c77011c1848604e732bacb54a9
Mihkel Vaher <mihkel.vaher@industry62.com>
2023-12-11 19:36:26 +0200

    restcontroller, repository, save, findall

---
+++ salat/salat/src/main/java/ee/mihkel/salat/SalatApplication.java
@@ -0,0 +1,13 @@
+package ee.mihkel.salat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalatApplication.class, args);
+	}
+
+}
---
+++ salat/salat/src/main/java/ee/mihkel/salat/entity/Toiduaine.java
@@ -0,0 +1,28 @@
+package ee.mihkel.salat.entity;
+// entity, model
+// Andmebaasimudelite jaoks
+// Nimistu objektidest
+
+// DTO - näitan front-endile teistmoodi kui andmebaasis (teeme tulevikus)
+
+import jakarta.persistence.Entity; // javax - vanasti
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor // Toiduaine toiduaine = new Toiduaine();
+@AllArgsConstructor // Toiduaine toiduaine = new Toiduaine("", 1,2,3);
+@Entity
+public class Toiduaine {
+    @Id
+    private String nimi;
+    private int rasv;
+    private int sysivesik;
+    private int valk;
+
+
+}
